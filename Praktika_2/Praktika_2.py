2f329aff976ec383b8a9755d490eee923b82f0aa
Kali01777 <mykali03@yandex.ru>
2022-10-26 13:58:56 +0300

    Change

--- Praktika_2/Praktika_2.py
+++ Praktika_2/Praktika_2.py
@@ -1,26 +1,71 @@
 import hashlib
+from threading import Thread
 
 hs1 = "1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad"
 hs2 = "3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b"
 hs3 = "74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f"
-slovar = []
+slovar = []#26
+ot1, ot2, ot3 = "", "", ""
 for i in range(97, 123):
     slovar.append(chr(i))
-for a1 in slovar:
-    for a2 in slovar:
-        for a3 in slovar:
-            for a4 in slovar:
-                for a5  in slovar:
-                    slovo = a1+a2+a3+a4+a5
-                    hash_object = hashlib.sha256(bytes(slovo, encoding = "utf-8"))
-                    hex_dig = hash_object.hexdigest()
-                    if hex_dig == hs1:
-                        ot1 = slovo
-                    if hex_dig == hs2:
-                        ot2 = slovo
-                    if hex_dig == hs3:
-                        ot3 = slovo
+while True:
+    v = int(input("Введите кол-во потоков (1-4 или 8) : "))
+    if 0<v<5 or v==8:
+        break
+def zero(x, l):
+    for a1 in slovar[x:l]:
+        for a2 in slovar[x:26]:
+            for a3 in slovar[x:26]:
+                for a4 in slovar[x:26]:
+                    for a5  in slovar[x:26]:
+                        slovo = a1+a2+a3+a4+a5
+                        hash_object = hashlib.sha256(bytes(slovo, encoding = "utf-8"))
+                        hex_dig = hash_object.hexdigest()
+                        if hex_dig == hs1:
+                            s1=hs1 + " = " + slovo
+                            print(s1)
+                        if hex_dig == hs2:
+                            s2 = hs2 + " = " + slovo
+                            print(s2)
+                        if hex_dig == hs3:
+                            s3 = hs1 + " = " + slovo
+                            print(s3)
 
-print(ot1, "=", hs1)
-print(ot2, "=", hs2)
-print(ot3, "=", hs3)
+
+if v == 1:
+    zero(0, 26)
+if v == 2:
+    thread_1 = Thread(target=zero, args=(0,13))
+    thread_2 = Thread(target=zero, args=(13, 26))
+    thread_1.start()
+    #thread_1.join()
+    thread_2.start()
+    #thread_2.join()
+if v == 3:
+    thread_1 = Thread(target=zero, args=(0,9))
+    thread_2 = Thread(target=zero, args=(9,18))
+    thread_3 = Thread(target=zero, args=(18,26))
+    thread_1.start()
+    #thread_1.join()
+    thread_2.start()
+    #thread_2.join()
+    thread_3.start()
+    #thread_3.join()
+if v == 4:
+    thread_1 = Thread(target=zero, args=(0, 7))
+    thread_2 = Thread(target=zero, args=(7, 14))
+    thread_3 = Thread(target=zero, args=(14, 20))
+    thread_4 = Thread(target=zero, args=(20, 26))
+    thread_1.start()
+    #thread_1.join()
+    thread_2.start()
+    #thread_2.join()
+    thread_3.start()
+    #thread_3.join()
+    thread_4.start()
+    #thread_4.join()
+"""
+3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b = apple
+74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f = mmmmm
+1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad = zyzzx
+"""
